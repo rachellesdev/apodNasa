b4cd051a4293f2beea2c35ce0ff745ef6d0f9286
Rachelle Santolin <rachelle.santolin@gmail.com>
2022-05-11 11:16:21 -0300

    inclui comentários ao código

--- js/scrip.js
+++ js/scrip.js
@@ -1,11 +1,13 @@
 // api key = 4BRmrLBBB1Cwrz53RdZAdK0Nxob5FFmUpZ2RflnM
 
+// adicionando um evento ao botão para rodar a função sendRequest ao ser clicado
 $(document).ready(function () {
     $("#btn").click(function (e) {
         e.preventDefault();
         sendRequest();
     });
 
+    // função assincrona que vai buscar os dados da API
     async function sendRequest() {
         const apiKey = "4BRmrLBBB1Cwrz53RdZAdK0Nxob5FFmUpZ2RflnM";
         const inData = $("#inData").val();
@@ -14,9 +16,11 @@ $(document).ready(function () {
         const data = await response.json();
         console.log(data);
 
+        // utilizando os dados encontrados na API
         document.querySelector('#tituloImg').innerHTML = data.title;
         document.querySelector('#explainImg').innerHTML = data.explanation;
 
+        // verificando se o tipo de midia encontrado é video ou imagem para saber se deve-se utilizar a tag img ou iframe
         if (data.media_type == "image") {
             document.querySelector('#imgNasa').innerHTML = `<br><img width="700px" height="500px" src="${data.url}">`;
         } else {
